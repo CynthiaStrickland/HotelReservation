7676eca82a5d6e435b57621c44e6f30fd020122b
Notsosof1 <cawhitlatch2@gmail.com>
2015-11-30 09:06:35 -0800

    Initial Commit

---
+++ HotelReservation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelReservation
+//
+//  Created by Cynthia Whitlatch on 11/30/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HotelReservation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HotelReservation
+//
+//  Created by Cynthia Whitlatch on 11/30/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
