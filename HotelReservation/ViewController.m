6f21a3c531296470b872607bd64f3079225e0543
Notsosof1 <cawhitlatch2@gmail.com>
2015-11-30 16:50:47 -0800

    Adding Constraints

--- HotelReservation/ViewController.m
+++ HotelReservation/ViewController.m
@@ -15,29 +15,92 @@
 @implementation ViewController
 
 - (void)loadView {
+    [super loadView];
     [self setupCustomLayout];
 }
 
 - (void)setupCustomLayout {
     
-    float navigationBarHeight = CGRectContainsPoint(self.navigationController.navigationBar.frame);
+    float navigationBarHeight = CGRectGetHeight(self.navigationController.navigationBar.frame);
+    
+        //Create three buttons
     UIButton *browseButton = [[UIButton alloc] init];
     UIButton *bookButton = [[UIButton alloc] init];
     UIButton *lookupButton = [[UIButton alloc] init];
     
+        //Set Button Titles
     [browseButton setTitle:@"Browse" forState:UIControlStateNormal];
     [bookButton setTitle:@"Book" forState:UIControlStateNormal];
     [lookupButton setTitle:@"Lookup" forState:UIControlStateNormal];
-
+    
+        //Set Button Background Colors
     [browseButton setBackgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:0.75 alpha:1.0]];
     [bookButton setBackgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:0.75 alpha:1.0]];
     [lookupButton setBackgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:0.75 alpha:1.0]];
 
-    [browseButton setTitleColor:[UIColor blackColor] forState:nil];
-    [bookButton setTitleColor:[UIColor blackColor] forState:nil];
-    [lookupButton setTitleColor:[UIColor blackColor] forState:nil];
+        //Set Button Title Colors
+    [browseButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [bookButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [lookupButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [browseButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [bookButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [lookupButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+            //Browse
+    NSLayoutConstraint *browseButtonLeading = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view  attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0];
+    
+    NSLayoutConstraint *browseButtonTrailing = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view  attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0];
+    
+    NSLayoutConstraint *browseButtonTop = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:64.0];
+    
+            //Book
+    NSLayoutConstraint *bookButtonLeading = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view  attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0];
+    
+    NSLayoutConstraint *bookButtonCenterY = [NSLayoutConstraint constraintWithItem:bookButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:navigationBarHeight / 1.4];
+    
+    NSLayoutConstraint *bookButtonTrailing = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0];
+    
+            //Lookup
+    NSLayoutConstraint *lookupButtonLeading = [NSLayoutConstraint constraintWithItem:lookupButton attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view  attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0];
+    
+    NSLayoutConstraint *lookupButtonTrailing = [NSLayoutConstraint constraintWithItem:lookupButton attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view  attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0];
+    
+    NSLayoutConstraint *lookupButtonBottom = [NSLayoutConstraint constraintWithItem:lookupButton attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
+    
+            //BUTTON HEIGHT
+    NSLayoutConstraint *browseButtonHeight = [NSLayoutConstraint constraintWithItem:browseButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *bookButtonHeight = [NSLayoutConstraint constraintWithItem:bookButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:0.3 constant:0.0];
+    NSLayoutConstraint *lookupButtonHeight = [NSLayoutConstraint constraintWithItem:lookupButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0.0];
+    
+    [self.view addSubview:browseButton];
+    [self.view addSubview:bookButton];
+    [self.view addSubview:lookupButton];
+    
+        //Activate Constraints
+    browseButtonLeading.active = YES;
+    browseButtonTrailing.active = YES;
+    browseButtonTop.active = YES;
+    
+    bookButtonLeading.active = YES;
+    bookButtonTrailing.active = YES;
+    bookButtonCenterY.active = YES;
+    
+    lookupButtonLeading.active = YES;
+    lookupButtonTrailing.active = YES;
+    lookupButtonBottom.active = YES;
+    
+    browseButtonHeight.active = YES;
+    bookButtonHeight.active = YES;
+    lookupButtonHeight.active = YES;
+    
+            //SETUP ACTIONS
+    
+    [browseButton addTarget:self action:@selector(browseButtonSelected:) forControlEvents:UIControlEventTouchUpInside];
+    [bookButton addTarget:self action:@selector(bookButtonSelected:) forControlEvents:UIControlEventTouchUpInside];
+    [lookupButton addTarget:self action:@selector(lookupButtonSelected:) forControlEvents:UIControlEventTouchUpInside];
 }
-                                      
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupViewController];
